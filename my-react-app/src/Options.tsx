6f2a9a4e64ed333901fc6d03931c7860ea1429aa
FRANK <developer71@azet.sk>
2026-07-17 16:10:19 +0200

    Clean up options component

--- my-react-app/src/Options.tsx
+++ my-react-app/src/Options.tsx
@@ -1,25 +1,26 @@
-import { EventHandler, useState } from "react";
+import { useState } from "react";
 
 function Options() {
+  const [selectedOption, setSelectedOption] = useState("First");
 
-    const [options, setOptions] = useState("test");
-  const change = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setOptions(event.target.value);
+  const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedOption(event.target.value);
   };
-  console.log(options);  
 
   return (
     <form>
-        
-      <select onChange={change} value={options}>
-          <option value="First">First</option>
-          <option value="Second">Second</option>
-          <option value="Third">Third</option>
-      </select>    
+      <label htmlFor="example-option">Example option</label>
+      <select
+        id="example-option"
+        onChange={handleChange}
+        value={selectedOption}
+      >
+        <option value="First">First</option>
+        <option value="Second">Second</option>
+        <option value="Third">Third</option>
+      </select>
     </form>
-    
   );
-  
 }
 
 export default Options;
